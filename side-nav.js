07a836e7218b92d06693f3098d25473dd0f5213a
jjjjjssss <jsminran@gmail.com>
2021-01-06 22:41:48 -0800

    Added intro sections to case studies, redid Art & More layout

--- side-nav.js
+++ side-nav.js
@@ -1,7 +1,7 @@
 $(window).load(function(){      
             var scroll_pos = 0;
 	    var top_offset = $(window).height() / 2;
-	    var art_pos = 0;
+	    var art_pos = $("#art-section").offset().top - 1;
 	    var yearbook_pos = $("#yearbook").offset().top - top_offset;
 	    var id_pos = $("#industrial-design").offset().top - top_offset;
 	    var other_pos = $("#other").offset().top - top_offset;
