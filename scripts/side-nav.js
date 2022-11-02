265936272c320b2388d2213b5c7754794da75ef8
jjjjjssss <jsminran@gmail.com>
2022-11-02 01:42:50 -0400

    Changes to art, add crochet picture

--- scripts/side-nav.js
+++ scripts/side-nav.js
@@ -12,26 +12,34 @@ $(window).load(function(){
 
                 if(scroll_pos >= art_pos && scroll_pos < yearbook_pos) {
                     $("#nav-art").css('color', '#363636');
+                    $("#nav-art").css('font-weight', '500');
                 } else {
-                    $("#nav-art").css('color', '#c4c4c4');
+                    $("#nav-art").css('color', '#909090');
+                    $("#nav-art").css('font-weight', '300');
                 }
 
                 if(scroll_pos >= yearbook_pos && scroll_pos < id_pos) {
                     $("#nav-yearbook").css('color', '#363636');
+                    $("#nav-yearbook").css('font-weight', '500');
                 } else {
-                    $("#nav-yearbook").css('color', '#c4c4c4');
+                    $("#nav-yearbook").css('color', '#909090');
+                    $("#nav-yearbook").css('font-weight', '300');
                 }
 
                 if(scroll_pos >= id_pos && scroll_pos < other_pos) {
                     $("#nav-id").css('color', '#363636');
+                    $("#nav-id").css('font-weight', '500');
                 } else {
-                    $("#nav-id").css('color', '#c4c4c4');
+                    $("#nav-id").css('color', '#909090');
+                    $("#nav-id").css('font-weight', '300');
                 }
 
                 if(scroll_pos >= other_pos) {
                     $("#nav-other").css('color', '#363636');
+                    $("#nav-other").css('font-weight', '500');
                 } else {
-                    $("#nav-other").css('color', '#c4c4c4');
+                    $("#nav-other").css('color', '#909090');
+                    $("#nav-other").css('font-weight', '300');
                 }
 
 
